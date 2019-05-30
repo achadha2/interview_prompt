2648e30b541092d7de1eb86e56c3f0131062b2d7
achadha2 <andy.chadha2@gmail.com>
2019-05-30 12:12:02 -0400

    finished basic functionality

--- src/Containers/Column.jsx
+++ src/Containers/Column.jsx
@@ -3,24 +3,75 @@ import React from "react";
 export default class Column extends React.Component {
   renderTasks = () => {
     return this.props.tasks.map(task => {
-      return <div className="task">{task.task}</div>;
+      if (this.props.owner.id === 1) {
+        return (
+          <div className="task">
+            <div>{task.task}</div>
+            <div
+              className="arrowright"
+              onClick={e => this.props.handleColumnChange(e, task)}
+              data-column="1"
+            >
+              +
+            </div>
+          </div>
+        );
+      } else if (this.props.owner.id === 4) {
+        return (
+          <div className="task">
+            <div
+              className="arrowleft"
+              onClick={e => this.props.handleColumnChange(e, task)}
+              data-column="-1"
+            >
+              -
+            </div>
+            <div>{task.task}</div>
+          </div>
+        );
+      } else {
+        return (
+          <div className="task">
+            <div
+              className="arrowleft"
+              onClick={e => this.props.handleColumnChange(e, task)}
+              data-column="-1"
+            >
+              -
+            </div>
+            <div>{task.task}</div>
+            <div
+              className="arrowright"
+              onClick={e => this.props.handleColumnChange(e, task)}
+              data-column="1"
+            >
+              +
+            </div>
+          </div>
+        );
+      }
     });
   };
 
+  // addTask = () => {
+  //   let result = window.prompt("hello please add task");
+  //   this.props.tasks.push({
+  //     ownerID: this.props.tasks[0].ownerID,
+  //     owner: this.props.tasks[0].owner,
+  //     task: result
+  //   });
+  //   console.log(this.props);
+  // };
+
   addTask = () => {
-    let result = window.prompt("hello please add task");
-    this.props.tasks.push({
-      ownerID: this.props.tasks[0].ownerID,
-      owner: this.props.tasks[0].owner,
-      task: result
-    });
-    console.log(this.props);
+    let result = window.prompt("Please type new task below:");
+    this.props.handleTaskAdd(this.props.owner.id, result);
   };
 
   render() {
     return (
       <div className="column">
-        <div className="columnHeader">{this.props.name}</div>
+        <div className="columnHeader">{this.props.owner.name}</div>
         <div>{this.renderTasks()}</div>
         <div onClick={this.addTask}>+ Add a card</div>
       </div>
--- src/Containers/ColumnContainer.jsx
+++ src/Containers/ColumnContainer.jsx
@@ -3,29 +3,64 @@ import Column from "./Column";
 
 export default class ColumnContainer extends React.Component {
   state = {
-    names: ["Bob", "Tim", "Jon", "Kim"],
+    owners: [
+      { id: 1, name: "Bob" },
+      { id: 2, name: "Kim" },
+      { id: 3, name: "Tim" },
+      { id: 4, name: "Jim" }
+    ],
     tasks: [
-      { ownerID: 1, owner: "Bob", task: "Go buy a lot of eggs" },
-      { ownerID: 1, owner: "Bob", task: "Sell your eggs" },
-      { ownerID: 2, owner: "Tim", task: "Buy a monocle" },
-      { ownerID: 2, owner: "Tim", task: "Finish report for work" },
-      { ownerID: 3, owner: "Jon", task: "Complete assignment number 2" },
-      { ownerID: 3, owner: "Jon", task: "Complete assignment number 4" },
-      { ownerID: 4, owner: "Kim", task: "Buy gifts for christmas" },
-      { ownerID: 4, owner: "Kim", task: "Fire employee X" }
+      { ownerID: 1, task: "Go buy a lot of eggs" },
+      { ownerID: 1, task: "Sell your eggs" },
+      { ownerID: 2, task: "Buy a monocle" },
+      { ownerID: 2, task: "Finish report for work" },
+      { ownerID: 3, task: "Complete assignment number 2" },
+      { ownerID: 3, task: "Complete assignment number 4" },
+      { ownerID: 4, task: "Buy gifts for christmas" },
+      { ownerID: 4, task: "Fire employee X" }
     ]
   };
 
   whatName = () => {
-    return this.state.names.map(name => {
+    return this.state.owners.map(owner => {
       let results = this.state.tasks.filter(task => {
-        return task.owner === name;
+        return task.ownerID === owner.id;
       });
-      console.log(results);
 
-      return <Column name={name} tasks={results} />;
+      return (
+        <Column
+          owner={owner}
+          tasks={results}
+          handleTaskAdd={this.handleTaskAdd}
+          handleColumnChange={this.handleColumnChange}
+        />
+      );
     });
   };
+
+  handleTaskAdd = (ownerID, task) => {
+    this.setState({
+      tasks: [...this.state.tasks, { ownerID: ownerID, task: task }]
+    });
+  };
+
+  handleColumnChange = (e, task) => {
+    let colChange = parseInt(e.target.dataset.column);
+    let newID = parseInt(task.ownerID) + colChange;
+    let newTask = { ownerID: newID, task: task.task };
+
+    let newArray = this.state.tasks.map(oldtask => {
+      if (oldtask.task === task.task) {
+        return { ...oldtask, ownerID: newID };
+      } else {
+        return oldtask;
+      }
+    });
+    this.setState({
+      tasks: newArray
+    });
+  };
+
   render() {
     return <div className="columnContainer">{this.whatName()}</div>;
   }
